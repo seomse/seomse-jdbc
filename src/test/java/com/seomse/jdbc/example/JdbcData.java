ca3db2ccd42fa63a81d09fd73fa94467fad1180a
macle86 <ysys86a@gmail.com>
2019-10-17 12:10:20 +0900

    Jdbc Objects (작업중)

--- src/test/java/com/seomse/jdbc/example/JdbcData.java
+++ src/test/java/com/seomse/jdbc/example/JdbcData.java
@@ -1,4 +1,30 @@
 package com.seomse.jdbc.example;
 
+import com.seomse.commons.packages.classes.field.FieldUtil;
+
+import java.lang.reflect.Field;
+
 public class JdbcData {
+    String test;
+    long logs;
+    Long longs;
+
+    public static void main(String[] args) {
+        Field[] fields = FieldUtil.getFieldArrayToParents(JdbcData.class);
+
+        for(Field field : fields){
+            if(Long.TYPE ==field.getType()){
+                System.out.println("롱타입");
+            }
+
+            if(String.class == field.getType()){
+                System.out.println("문자열타입");
+            }
+
+            System.out.println(field.getName() + ", " + field.getType());
+        }
+    }
+
+
+
 }
